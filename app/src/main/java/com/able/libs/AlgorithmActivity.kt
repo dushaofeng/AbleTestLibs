8dbbf390f06e87be0c0e9b2e7f44bbb2f4fd9e88
dushaofeng <dushaofeng021@163.com>
2017-08-30 16:58:43 +0800

    优化算法的Kotlin结构

--- app/src/main/java/com/able/libs/AlgorithmActivity.kt
+++ app/src/main/java/com/able/libs/AlgorithmActivity.kt
@@ -7,28 +7,44 @@ import android.view.View
 import com.able.libs.R.layout.algorithm_layout
 import kotlinx.android.synthetic.main.algorithm_layout.*
 import java.util.*
+import kotlin.concurrent.thread
 
 /**
  * Created by Able on 2017/8/29.
  * 测试各种算法的效率
  */
-class AlgorithmActivity : AppCompatActivity(), View.OnClickListener {
-    private val ARRAY_COUNT = 10*10000
+class AlgorithmActivity : AppCompatActivity() {
+    private val ARRAY_COUNT = 10 * 1000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(algorithm_layout)
-        bt_maopao.setOnClickListener(this)
     }
 
-    override fun onClick(v: View?) {
-        Maopao()
+    fun click(view: View) {
+        when (view) {
+        //冒泡算法
+            bt_maopao -> {
+                bt_maopao.text = "正在计算..."
+                thread { maoPao() }
+            }
+            bt_xuanze -> {
+                bt_xuanze.text = "正在计算..."
+                thread { xuanZe() }
+            }
+            bt_charu -> {
+                bt_charu.text = "正在计算..."
+                thread { chaRu() }
+            }
+            else -> {
+                Log.d("ssss", "Error")
+            }
+        }
     }
-
 
     /*
     冒泡算法
      */
-    fun Maopao() {
+    fun maoPao() {
         val start = System.currentTimeMillis()
         val sortList = IntArray(ARRAY_COUNT)
         val ra = Random()
@@ -46,6 +62,20 @@ class AlgorithmActivity : AppCompatActivity(), View.OnClickListener {
             }
         }
         val end = System.currentTimeMillis()
-        Log.d("ssss", "耗时：" + (end - start))
+        runOnUiThread { bt_maopao.text = "冒泡算法耗时：" + (end - start) / 1000 + "秒" }
+    }
+
+    /*
+    插入算法
+     */
+    fun chaRu() {
+
+    }
+
+    /*
+    选择算法
+     */
+    fun xuanZe() {
+
     }
 }
