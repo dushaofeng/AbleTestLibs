38ad8fae735570523da07ab8f12523f874b3d724
dushaofeng <dushaofeng021@163.com>
2017-09-25 10:57:13 +0800

    Add application

---
+++ app/src/main/java/com/able/libs/AppApplication.java
@@ -0,0 +1,14 @@
+package com.able.libs;
+
+import android.app.Application;
+
+/**
+ * Created by Able on 2017/9/25.
+ */
+
+public class AppApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
