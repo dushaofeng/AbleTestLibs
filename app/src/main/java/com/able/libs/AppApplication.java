32230d21462f863f7e30dda3414197b5ce93c6a8
dushaofeng <dushaofeng021@163.com>
2017-11-03 16:33:49 +0800

    Custom font successful

--- app/src/main/java/com/able/libs/AppApplication.java
+++ app/src/main/java/com/able/libs/AppApplication.java
@@ -5,6 +5,8 @@ import android.app.Application;
 import com.raizlabs.android.dbflow.config.FlowConfig;
 import com.raizlabs.android.dbflow.config.FlowManager;
 
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
 /**
  * Created by Able on 2017/9/25.
  */
@@ -13,6 +15,11 @@ public class AppApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/Roboto-ThinItalic.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
         FlowManager.init(this);
     }
 }
--- app/src/main/java/com/able/libs/MainActivity.java
+++ app/src/main/java/com/able/libs/MainActivity.java
@@ -1,16 +1,24 @@
 package com.able.libs;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
 /**
  * Created by Able on 2017/10/15.
  */
 @EActivity(R.layout.super_main_layout)
 public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
     @Click
     void gotoKotlinAndBezier() {
         startActivity(new Intent(this, KotlinAndBezierTestActivity.class));
